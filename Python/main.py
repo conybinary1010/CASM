642c5540bd719dac6c55dde8c544cc83317b9ee2
conybinary1010-git <conybinary1010@gmail.com>
2026-09-16 17:52:49 +0200

    FIxed Python logic

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,7 @@
 
+from asyncio.windows_events import NULL
+
+
 instruction_set = {
     "HALT" : 0x01,
     "LOAD" : 0x2,
@@ -13,34 +16,38 @@ reg_set = {
 
 
 def assemble_instructions(instruction, reg, value):
-    if instruction not in instruction_set:
+    if instruction not in instruction_set and instruction is not NULL:
         raise ValueError(f"invalid instruction: {instruction}")
-    elif reg not in reg_set:
+    elif reg not in reg_set and reg is not NULL :
         raise ValueError(f"invalid reg : {reg}")
 
-    machine_code = []
+    machine_code_instr = []
+    machine_code_reg = []
+    machine_code_value = []
 
     if  instruction == "HALT":
-        machine_code.append(instruction_set[instruction])
-        return machine_code
+        machine_code_instr.append(instruction_set[instruction])
+        return machine_code_instr
     elif  instruction == "LOAD":
-        machine_code.append(instruction_set[instruction])
-        return machine_code
+        machine_code_instr.append(instruction_set[instruction])
+        return machine_code_instr
     elif  instruction == "READ":
-        machine_code.append(instruction_set[instruction])
-        return machine_code
+        machine_code_instr.append(instruction_set[instruction])
+        return machine_code_instr
 
     elif reg == "r0":
-        machine_code.append(reg_set[reg])
-        return machine_code
+        machine_code_reg.append(reg_set[reg])
+        return machine_code_reg
     elif reg == "r1":
-        machine_code.append(reg_set[reg])
-        return machine_code
+        machine_code_reg.append(reg_set[reg])
+        return machine_code_reg
     elif reg == "r2":
-        machine_code.append(reg_set[reg])
-        return machine_code
+        machine_code_reg.append(reg_set[reg])
+        return machine_code_reg
 
     elif value is hex:
-        machine_code.append(int(value, 16))
+        machine_code_value.append(int(value, 16))
 
-print("macine code:", assemble_instructions("HALT", "r0", "0x00"))
+print("macine code:", assemble_instructions("LOAD", NULL, NULL))
+print("macine code:", assemble_instructions(NULL, "r0", NULL))
+print("macine code:", assemble_instructions(NULL, NULL, "0x00"))
