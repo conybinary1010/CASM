597698cdd23519762542d1f858aeacc64ddcd50a
conybinary1010-git <conybinary1010@gmail.com>
2026-09-21 16:08:43 +0200

    Added file output

--- main.c
+++ main.c
@@ -18,9 +18,28 @@ uint8_t assembly_instruction(uint8_t instruction, uint8_t reg, uint8_t value){
     return machine_code;
 }
 
-int main(){
-    assembly_instruction(LOAD, r0, 20);
-    assembly_instruction(READ, r0, 0);
-    assembly_instruction(HALT, 0, 0);
+int file_output(){
+    FILE *out = fopen("program.bin", "wb");
+    if (!out){
+        perror("File errror");
+        return 0;
+    }
+    
+    uint16_t instru1 = assembly_instruction(LOAD, r0, 20);
+    uint16_t instru2 = assembly_instruction(READ,r0, 0);
+    uint16_t instru3 = assembly_instruction(HALT, 0, 0);
+
+    if (fwrite(&instru1, sizeof(uint16_t), 1, out) != 1){
+        perror("error writing to file");
+        fclose(out);
+        return 1;
+    }
+   
+    fclose(out);
+    printf("program succesfuly assembled to program.bin\n");
     return 0;
+}
+
+int main(){
+    return file_output();
 }
